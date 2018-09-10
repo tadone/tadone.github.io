f174a92c1aa1c5536f06628d5bc3a7efe1968f66
Tad Swider <tadeusz.swider@motorolasolutions.com>
2018-09-10 11:17:39 -0600

    add echo lines

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,19 @@ set -e
 git config user.name $GH_NAME
 git config user.email $GH_EMAIL
 
+echo "checkout master"
 git checkout master
-git pull origin master
+#git pull origin master
 
+echo "delete all files except .git and vuepress"
 find . -maxdepth 1 ! -name 'vuepress' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
 mv vuepress/* .
 rm -R vuepress/
 
 git add -fA
 git commit --allow-empty -m "$(git log develop -1 --pretty=%B)"
-git push -f origin $TARGET_BRANCH
+
+echo "push to master"
+git push -f origin master
 
 echo "deployed successfully"
